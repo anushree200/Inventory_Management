c608cb88914a2151bc6aeb56c185d8967ae190cf
anushree200 <aanuushree@gmail.com>
2025-04-06 21:20:59 +0530

    new plans

--- setup_db.py
+++ setup_db.py
@@ -7,12 +7,13 @@ cursor = conn.cursor()
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS users (
     username TEXT PRIMARY KEY,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    phoneno INTEGER
 )
 ''')
 
 # Insert demo user
-cursor.execute("INSERT OR IGNORE INTO users (username, password) VALUES (?, ?)", ("admin", "admin123"))
+cursor.execute("INSERT OR IGNORE INTO users (username, password, phoneno) VALUES (?, ?, ?)", ("anu", "anu123", 8939596811))
 
 # Products table
 cursor.execute('''
@@ -20,13 +21,41 @@ CREATE TABLE IF NOT EXISTS products (
     pid INTEGER PRIMARY KEY AUTOINCREMENT,
     pname TEXT NOT NULL,
     category TEXT,
-    description TEXT,
+    size INTEGER,
     qty INTEGER NOT NULL,
+    minqty INTEGER NOT NULL,
     price INTEGER NOT NULL,
-    supplier TEXT,
-    date_added TEXT,
-    image TEXT,
-    status TEXT
+    barcode INTEGER NOT NULL
+)
+''')
+
+# Vendor table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS vendor(
+    pid INTEGER PRIMARY KEY,
+    pname TEXT NOT NULL,
+    vendorid INTEGER NOT NULL,
+    vendorname TEXT,
+    contactno INTEGER,
+    email TEXT,
+    address TEXT
+)
+''')
+
+# Stock table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS stock(
+    vendorid INTEGER PRIMARY KEY,
+    vendorname TEXT,
+    pid INTEGER NOT NULL,
+    pname TEXT,
+    sellingprice INTEGER,
+    stockeddate DATE,
+    restockingdate DATE,
+    expirationdate DATE,
+    qntypresent INTEGER,
+    qntysold INTEGER,
+    description TEXT
 )
 ''')
 
