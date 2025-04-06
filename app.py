7b8fdad211170c23abba8ebc6911ed8569478fe7
anushree200 <aanuushree@gmail.com>
2025-04-06 18:44:46 +0530

    modified again file name

--- app.py
+++ app.py
@@ -1,33 +1,76 @@
-import Inventory_Management.db_utils as db_utils
-print ("Welcome to Inventory management")
-uname = input("Enter username:")
-pwd = input("Enter password:")
-result = db_utils.login(uname, pwd)
-if(result == "Success"):
-    db_utils.displayData ()
-    choice = int(input("Choose one of the following\nl. Add new data\n2. Remove data\n3. Update data\n4. Display data\n5. Exit\n"))
-    while (choice >0 and choice <5):
-      if choice==1:
-        product_name = input("enter product name: ")
-        qty = int(input("enter quantity:"))
-        price = int(input("enter price:") )
-        db_utils.insertData(product_name,price,qty)
-      elif choice == 2:
-        product_name= input("enter product name:")
-        db_utils.deletedata(product_name)
-      elif choice==3:
-        name = input("Enter product name you would like to update:")
-        option=int(input("do you want to change 1. quantity\n2.price\n"))
-        if option==1:
-          qty=int(input("enter the new quantity:")) 
-          db_utils.updateData(name, qty, 0)
-        elif option==2:
-          price=int(input("enter the new price:"))
-          db_utils.updateData(name, 0, price)
-        else:
-          print("Invalid choice")
-      elif choice==4:
-        db_utils.displayData()
-      choice = int(input("Choose one of the following\nl. Add new data\n2. Remove data\n3. Update data\n4. Display data\n5. Exit\n"))
-else:
-  print("please check the details")
+import sqlite3
+def login(uname, pwd):
+  value = ""
+  try:
+    con = sqlite3.connect ("database.db")
+    cursor = con.cursor()
+    cursor.execute(f"select * from users where username = '(uname)'")
+    records = cursor.fetchall()
+    if len (records)==1:
+      if pwd == records [0] [1]:
+        value = "Success"
+      else:
+        value = "Incorrect password"
+    else:
+      value = "Please check the username"
+    cursor.close()
+    con.close()
+    return value
+  except:
+    print ("Database error")
+def displayData():
+  try:
+    con = sqlite3.connect("database.db")
+    cursor = con.cursor()
+    cursor.execute(f"select * from products")
+    records = cursor.fetchall()
+    for i in records:
+      print(f'{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}')
+    cursor.close()
+    con.close()
+  except:
+    print("Database error")
+def insertData(name, price, qty):
+  try:
+    con = sqlite3.connect("database.db")
+    cursor = con.cursor()
+    cursor.execute("select MAX(pid) from products")
+    records = cursor.fetchall()
+    id_new = records[0][0]+1
+    query = f"insert into products(pid, pname, qty,price) values({id_new},'{name}','{qty}','{price}')"
+    i = cursor.execute(query)
+    print (f"{cursor.rowcount} row added successfully")
+    con.commit()
+    cursor.close()
+    con.close()
+  except:
+    print("Database error")
+
+def deletedata(product_name) :
+  try:
+    con = sqlite3.connect("database.db" )
+    cursor = con.cursor()
+    query = f"delete from products where pname='{product_name}'"
+    i = cursor.execute(query)
+    print (f"{cursor.rowcount} row deleted successfully")
+    con.commit()
+    cursor.close()
+    con.close()
+  except:
+    print("Database error")
+
+def updateData(name, qty, price):
+  try:
+    con = sqlite3.connect("database.db")
+    cursor = con.cursor()
+    if (qty>0) :
+      query = f"update products set qty = {qty} where pname='{name}'"
+    else:
+      query = f"update products set price = {price} where pname='{name}'"
+    i = cursor.execute(query)
+    print (f"{cursor.rowcount} row updated successfully")
+    con.commit()
+    cursor.close()
+    con.close()
+  except:
+    print ("Database error")
