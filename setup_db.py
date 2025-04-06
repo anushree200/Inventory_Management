ca70c59dadc4677bd8909d7790487d8368bb5b84
anushree200 <aanuushree@gmail.com>
2025-04-06 20:01:42 +0530

    modified stuff

--- setup_db.py
+++ setup_db.py
@@ -3,6 +3,7 @@ import sqlite3
 conn = sqlite3.connect("inventory.db")
 cursor = conn.cursor()
 
+# Users table
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS users (
     username TEXT PRIMARY KEY,
@@ -10,14 +11,22 @@ CREATE TABLE IF NOT EXISTS users (
 )
 ''')
 
-cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", ("admin", "admin123"))
+# Insert demo user
+cursor.execute("INSERT OR IGNORE INTO users (username, password) VALUES (?, ?)", ("admin", "admin123"))
 
+# Products table
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS products (
     pid INTEGER PRIMARY KEY AUTOINCREMENT,
     pname TEXT NOT NULL,
+    category TEXT,
+    description TEXT,
     qty INTEGER NOT NULL,
-    price INTEGER NOT NULL
+    price INTEGER NOT NULL,
+    supplier TEXT,
+    date_added TEXT,
+    image TEXT,
+    status TEXT
 )
 ''')
 
